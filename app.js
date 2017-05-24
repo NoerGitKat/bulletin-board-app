67e57ebcb243af8f11949dd81437600d77a9e8ce
Noer <noerpaanakker@hotmail.com>
2017-05-24 17:56:32 +0200

    expanded app.js, created routes and pug files with content

--- app.js
+++ app.js
@@ -5,6 +5,21 @@ const app = express();
 app.set('views', 'views');
 app.set('view engine', 'pug');
 
+// Route for homepage
+app.get('/', (request, response) => {
+	response.render('index');
+});
+
+// Route with form for posting message
+app.get('/post_message', (request, response) => {
+	response.render('post_message');
+});
+
+// Post request that handles the new input
+app.post('/post_message', (request, response) => {
+	response.render('message_posted');
+});
+
 const listener = app.listen(3000, () => {
 	console.log('The server has started at port:' + listener.address().port);
-})
+});
