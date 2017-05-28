6b2016f63fc691c80bbf2c9ca707532c9ccce163
Noer <noerpaanakker@hotmail.com>
2017-05-28 23:08:02 +0200

    added pg connection in app.post route. doesnt work yet

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const pg = require('pg');
 const app = express();
+const bodyParser = require('body-parser');
+
+//Database connection
+var connection = "postgres://postgres:Blabla_55@localhost/bulletin_board";
 
 app.set('views', 'views');
 app.set('view engine', 'pug');
@@ -19,7 +23,28 @@ app.get('/post_message', (request, response) => {
 
 // Post request that handles the new input
 app.post('/post_message', (request, response) => {
-	response.render('message_posted');
+	var message = [];
+
+	pg.connect(connection, (err, client, done) => {
+		if(err) {
+			return console.log('Beep boop an error has occurred: ', err);
+		}
+		client.query('INSERT INTO messages (title, body) VALUES ($1, $2)', 
+			[request.body.title, request.body.body]);
+			for (var i = 0; i < result.length; i++) {
+
+	  			// Create an object to save current row's data
+		  		var newMessage = {
+		  			'title':result[i].title,
+					'body':result[i].body,
+				}
+
+				message.push(newMessage);
+			}
+
+			response.render('message_posted', {messages: message});
+			done();
+	});
 });
 
 const listener = app.listen(3000, () => {
